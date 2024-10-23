f932a7cb180d2ea44c91482f9c24abdd2749e376
Shamail Ahmad <125307010+Shumail97@users.noreply.github.com>
2024-10-23 09:09:52 +0100

    Jquery.js

--- Jquery.js
+++ Jquery.js
@@ -1,1 +1,14 @@
-
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $("#fadeOutButton").click(function() {
+                $("#fadeOutText").fadeOut();
+            });
+            $("#appendButton").click(function() {
+                $("#appendText").append(" Appended text.");
+            });
+            $("#changeColorButton").click(function() {
+                $("#changeColorText").css("color", "red");
+            });
+        });
+    </script>
